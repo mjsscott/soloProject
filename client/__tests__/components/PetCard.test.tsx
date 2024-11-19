e8ec45fc8ab164e7ddd439e83b95f26d5beedb47
Adil Sunil <adilsunil66@gmail.com>
2024-11-19 17:39:34 +0100

    feat:MapComponent tests done

--- client/__tests__/components/PetCard.test.tsx
+++ client/__tests__/components/PetCard.test.tsx
@@ -1,33 +1,45 @@
-import { render, screen } from '@testing-library/react';
-import PetCard from '../../src/components/PetCard';
-import { MemoryRouter } from 'react-router-dom';
+import { render, screen } from "@testing-library/react";
+import PetCard from "../../src/components/PetCard";
+import { MemoryRouter } from "react-router-dom";
 
 const mockPet = {
-    _id: '1234',
-    name: 'Ginger',
-    type: 'Cat',
-    age: 3,
-    location: { lat: 53.8008, lng: -1.5491 },
-    city: 'Leeds',
-    shelterName: 'Shelter',
-    phone: '123456789',
-    email: 'shelter@gamil.com',
-    image: '/images/ginger.jpg',
-    gender: 'Female',
-    favorite: false,
-    breed: 'English longhair',
-    description: 'she is so sweet!',
-    available: true,
+  _id: "1234",
+  name: "Ginger",
+  type: "Cat",
+  age: 3,
+  location: { lat: 53.8008, lng: -1.5491 },
+  city: "Leeds",
+  shelterName: "Shelter",
+  phone: "123456789",
+  email: "shelter@gamil.com",
+  image: "/images/ginger.jpg",
+  gender: "Female",
+  favorite: false,
+  breed: "English longhair",
+  description: "she is so sweet!",
+  available: true,
 };
 
-describe('PetCard', () => {
-    it('renders pet information correctly', () => {
-        render(
-            <MemoryRouter>
-                <PetCard pet={mockPet} />
-            </MemoryRouter>
-        );
-        expect(screen.getByText(mockPet.name).textContent).toBe('Ginger')
+beforeAll(() => {
+  jest.spyOn(console, "warn").mockImplementation((message) => {
+    if (message.includes("React Router Future Flag Warning")) {
+      return;
+    }
+    console.warn(message);
+  });
+});
 
+afterAll(() => {
+  jest.restoreAllMocks();
+});
 
-})})
+describe("PetCard", () => {
+  it("renders pet information correctly", () => {
+    render(
+      <MemoryRouter>
+        <PetCard pet={mockPet} />
+      </MemoryRouter>
+    );
+    expect(screen.getByText(mockPet.name).textContent).toBe("Ginger");
+  });
+});
